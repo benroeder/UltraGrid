benroeder/UltraGrid

commit 4d4b166e2278d910c05266c6fed151ff14bcc9c5

GUI: Open the window as soon as possible
With this change the window should appear immediately. It will still be
unusable and frozen until all capababilities are queried, but should at
least give the user some indication that something is happening.

gui/QT/main.cpp
@@ -35,6 +35,9 @@ int main(int argc, char *argv[]){
 	//important: If this line is removed parsing float numbers for vu meter fails
 	std::setlocale(LC_NUMERIC, "C");
 
+	UltragridWindow uw;
+	uw.show();
+
 	process.start(ultragridExecutable, QStringList());
 	if(process.waitForStarted(1000) == false) {
 		QMessageBox msgBox;
@@ -64,8 +67,7 @@ int main(int argc, char *argv[]){
 	fmt.setProfile(QSurfaceFormat::CoreProfile);
 	QSurfaceFormat::setDefaultFormat(fmt);
 
-	UltragridWindow uw;
-	uw.show();
+	uw.initializeUgOpts();
 	return app.exec();
 }
 

gui/QT/window/ultragrid_window.cpp
@@ -54,25 +54,11 @@ QStringList argStringToList(const std::string& argString){
 
 } //anonymous namespace
 
-UltragridWindow::UltragridWindow(QWidget *parent): QMainWindow(parent){
-	ui.setupUi(this);
-	//ui.terminal->setVisible(false);
-#ifdef DEBUG
-	ui.actionTest->setVisible(true);
-#endif //DEBUG
-
+void UltragridWindow::initializeUgOpts(){
 	ultragridExecutable = UltragridWindow::findUltragridExecutable();
-
-#ifndef __linux
-	ui.actionUse_hw_acceleration->setVisible(false);
-#endif
 
 	previewTimer.setSingleShot(true);
 	connect(&previewTimer, SIGNAL(timeout()), this, SLOT(startPreview()));
-
-	//connect(sourceOption, SIGNAL(changed()), this, SLOT(schedulePreview()));
-	//connect(audioSrcOption, SIGNAL(changed()), this, SLOT(schedulePreview()));
-
 	setupPreviewCallbacks();
 
 	availableSettings.queryAll(ultragridExecutable.toStdString());
@@ -106,6 +92,18 @@ UltragridWindow::UltragridWindow(QWidget *parent): QMainWindow(parent){
 	processStatus.setText("UG: Stopped");
 	ui.statusbar->addWidget(&processStatus);
 	ui.statusbar->addWidget(&previewStatus);
+}
+
+UltragridWindow::UltragridWindow(QWidget *parent): QMainWindow(parent){
+	ui.setupUi(this);
+	//ui.terminal->setVisible(false);
+#ifdef DEBUG
+	ui.actionTest->setVisible(true);
+#endif //DEBUG
+
+#ifndef __linux
+	ui.actionUse_hw_acceleration->setVisible(false);
+#endif
 
 	QString verString(GIT_CURRENT_SHA1);
 	verString += GIT_CURRENT_BRANCH;

gui/QT/window/ultragrid_window.hpp
@@ -20,6 +20,8 @@ public:
 	UltragridWindow(QWidget *parent = 0);
 	static QString findUltragridExecutable();
 
+	void initializeUgOpts();
+
 protected:
 	void closeEvent(QCloseEvent *);
 

commit d51c60d5360ffc3bd0049a6429c613d6df4159fa

GUI: Set default surface format before creating window
This fixes "OpenGL 3.3 needed" error on MacOS introduced in
4d4b166e2278d910c05266c6fed151ff14bcc9c5

gui/QT/main.cpp
@@ -35,6 +35,13 @@ int main(int argc, char *argv[]){
 	//important: If this line is removed parsing float numbers for vu meter fails
 	std::setlocale(LC_NUMERIC, "C");
 
+	QSurfaceFormat fmt;
+	fmt.setDepthBufferSize(24);
+	fmt.setStencilBufferSize(8);
+	fmt.setVersion(3, 3);
+	fmt.setProfile(QSurfaceFormat::CoreProfile);
+	QSurfaceFormat::setDefaultFormat(fmt);
+
 	UltragridWindow uw;
 	uw.show();
 
@@ -59,13 +66,6 @@ int main(int argc, char *argv[]){
 		msgBox.exec();
 		return 1;
 	}
-
-	QSurfaceFormat fmt;
-	fmt.setDepthBufferSize(24);
-	fmt.setStencilBufferSize(8);
-	fmt.setVersion(3, 3);
-	fmt.setProfile(QSurfaceFormat::CoreProfile);
-	QSurfaceFormat::setDefaultFormat(fmt);
 
 	uw.initializeUgOpts();
 	return app.exec();